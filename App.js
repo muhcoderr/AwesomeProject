b6891e2964d2531e4b9e89b54366adeb95ff88c4
muhcoderr <muhammad4948@gmail.com>
2024-03-25 00:34:46 +0500

    Added - Elevated Cards

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import FlatCards from "./components/FlatCards";
+import ElevatedCards from "./components/ElevatedCards";
 
 export default function App() {
   return (
     <SafeAreaView>
       <ScrollView>
         <FlatCards />
+        <ElevatedCards/>
       </ScrollView>
     </SafeAreaView>
   );
